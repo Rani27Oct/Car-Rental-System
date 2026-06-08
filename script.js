356e794e2164448757e8f74317db8b413e05f27f
Rani27Oct <ranipalle40@gmail.com>
2026-06-08 23:20:08 +0530

    Refactor car booking and search functions

--- script.js
+++ script.js
@@ -1,5 +1,61 @@
 function bookCar(carName){
 
-    alert("✅ Booking Confirmed for " + carName);
+    document.getElementById("selectedCar").value =
+    carName;
 
+    window.scrollTo({
+        top:document.body.scrollHeight,
+        behavior:"smooth"
+    });
+}
+
+function confirmBooking(){
+
+    let customer =
+    document.getElementById("customerName").value;
+
+    let car =
+    document.getElementById("selectedCar").value;
+
+    if(customer === "" || car === ""){
+
+        alert("Please fill booking details!");
+        return;
+    }
+
+    alert(
+        "✅ Booking Confirmed!\n\nCustomer: "
+        + customer +
+        "\nCar: " +
+        car
+    );
+
+    document.getElementById("customerName").value="";
+    document.getElementById("selectedCar").value="";
+}
+
+function searchCar(){
+
+    let search =
+    document.getElementById("search")
+    .value
+    .toLowerCase();
+
+    let cards =
+    document.querySelectorAll(".car-card");
+
+    cards.forEach(card => {
+
+        if(
+            card.innerText
+            .toLowerCase()
+            .includes(search)
+        ){
+            card.style.display="block";
+        }
+        else{
+            card.style.display="none";
+        }
+
+    });
 }
